4c346b98e99d88a7cf4637ee3cc9ed8358541ccb
Siddhant Goswami <goswami.siddh@gmail.com>
2024-08-09 17:26:40 +0530

    Added notion file

--- src/main.py
+++ src/main.py
@@ -1,17 +1,37 @@
-from pydantic import BaseModel
-from fastapi import FastAPI
-
-app = FastAPI()
-# 1. Define the blueprint for APIs
-class Numbers(BaseModel):
-    x: int
-    y: int
-
-# 2. Create the API endpoint
-@app.post("/add")
-def add_numbers_api(numbers: Numbers):
-    return {"result": add_numbers(numbers.x, numbers.y)}
+import os
+from dotenv import load_dotenv
+from notion_client import Client 
 
 
-def add_numbers(x, y):
-    return x + y
+load_dotenv()
+
+notion = Client(auth=os.getenv("NOTION_API_KEY"))
+
+page_id = '67dd09be5b3b44d694996a75c07ecc26'
+
+response = notion.pages.retrieve(page_id=page_id)
+
+response_body = notion.blocks.children.list(
+    block_id=page_id
+)
+
+print(response_body)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import streamlit as st
+
+# st.title(response['properties']['title']['title'][0]['plain_text'])
+
+# st.write(response_body['results'][1]['paragraph']['rich_text'][0]['plain_text'])
