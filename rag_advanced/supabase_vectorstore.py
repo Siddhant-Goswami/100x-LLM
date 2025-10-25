e229bc27c196733df495c3184bb9e9edcdda23d9
Siddhant Goswami <goswami.siddh@gmail.com>
2025-10-25 22:53:53 +0530

    Separated RAG and advanced RAG:C5

--- rag_advanced/supabase_vectorstore.py
+++ rag_advanced/supabase_vectorstore.py
@@ -3,14 +3,8 @@ from llama_index.core import SimpleDirectoryReader, Document, StorageContext
 from llama_index.core import VectorStoreIndex
 from llama_index.vector_stores.supabase import SupabaseVectorStore
 import textwrap
-import os
 from dotenv import load_dotenv
 load_dotenv()
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-
-
-
-
 
 documents = SimpleDirectoryReader("./data/paul_graham/").load_data()
 
@@ -23,13 +17,10 @@ vector_store = SupabaseVectorStore(
 
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
-index = VectorStoreIndex.from_documents(
-    documents, storage_context=storage_context
-)
+index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
 
 query_engine = index.as_query_engine()
 
 response = query_engine.query("What did the author do growing up?")
 
 print(textwrap.fill(str(response), 100))
-
